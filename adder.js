75bd1667cab94c985a232fa6105087aeb1bf2b0c
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-02-03 16:51:50 -0600

    update adder.js

--- adder.js
+++ adder.js
@@ -6,7 +6,6 @@ let num2 = 26;
 
 let sum = num1 + num2;
 
-console.log(" The sum of num1 and num2 is: " + sum);
 console.log("\n")
 console.log(" The sum of " + num1 + " and " + num2 + " is " + sum);
 
