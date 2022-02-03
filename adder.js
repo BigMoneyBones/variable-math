d024b8536296dcd6a58c871da5c4c679be318459
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-02-03 14:38:10 -0600

    create and submit adder.js

---
+++ adder.js
@@ -0,0 +1,12 @@
+console.log("now im gonna add some stuff");
+
+let num1 = 16;
+
+let num2 = 26;
+
+let sum = num1 + num2;
+
+console.log(" The sum of num1 and num2 is: " + sum);
+console.log("\n")
+console.log(" The sum of " + num1 + " and " + num2 + " is " + sum);
+
