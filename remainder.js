7618d0382cf662fc974a49003991deab9495adc8
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-02-03 16:52:36 -0600

    update remainder.js

--- remainder.js
+++ remainder.js
@@ -4,10 +4,12 @@ let apos = 16;
 
 let bpos = 22;
 
+console.log("\n");
+
 console.log("If apos = 16 and bpos = 22,");
-
-console.log("The remainder of 22 divided 16 is 6");
-
+console.log("\n");
+console.log("The remainder of 22 divided by 16 is 6");
+console.log("\n");
 console.log("bpos % apos" + " =");
-
+console.log("\n");
 console.log(apos % bpos);
