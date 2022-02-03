e55650a0f80049e427e079e31ee09d81c2a16420
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-02-03 16:59:09 -0600

    Alternative.. simpler... possibly more correct solution..

---
+++ distance2.js
@@ -0,0 +1,12 @@
+console.log("Calculating distance between two points using javascript")
+
+let x1 = 2
+let y1 = 4
+let x2 = 3
+let y2 = 5
+
+let D = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2))
+console.log("\n");
+console.log("The distance between points (" + x1 + " , " + y1 + ") and (" + x2 + " , " + y2 + ") is equal to " + D + "." );
+console.log("\n");
+
